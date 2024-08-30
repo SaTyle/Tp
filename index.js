1931a945229cac1790d5e928776ab8581e0acc17
Ajit Shinde <97863462+SaTyle@users.noreply.github.com>
2024-08-30 20:33:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-@echo off
-rem Run the first command in the first terminal
-start cmd /k "ngrok config add-authtoken 2Itaz2ZYiBkGiO2XRTCEo0JAect_3kxzvcFpBToo5RFdZ8Cjo && ngrok http --domain=factual-destined-foxhound.ngrok-free.app 5000"
+// app.js
+const express = require('express');
 
-rem Wait for 1 second before starting the second terminal
-timeout /t 5 >nul
+const cors = require('cors');
 
-rem Run the second command in the second terminal
-start cmd /k "ngrok config add-authtoken 2lN8mjCUZrrMNPCyZ2JwUJ1NzjH_6SyzYmPWgANFo8VBACTjw && ngrok http --domain=delicate-happy-cub.ngrok-free.app 8000"
+// const { sequelize } = require('./models');
+const { sequelize } = require('./config/config'); // Correct
+
+
+const authRoutes = require('./routes/auth');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Express is running');
+});
+
+app.use('/auth', authRoutes);
+
+// const PORT = process.env.PORT || 5000;
+const PORT = 5000;
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+    await sequelize.sync();
+});
